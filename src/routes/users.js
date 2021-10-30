c3a20c4946f32688b6e94f54e0fbd541da29cae6
AndresVillamil <bcl.evillamil@gmail.com>
2021-10-30 15:47:08 -0500

    ajuste

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 const router = Router();
 
+//fuciona con version npm i node-fetch@2.6.1
+const fetch = require('node-fetch');
 
 
 
-router.get("/",   (req, res) => {
-
-    res.send('users');
+router.get("/",  async (req, res) => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+    res.json(users);
+   
 });
 
 module.exports = router;
